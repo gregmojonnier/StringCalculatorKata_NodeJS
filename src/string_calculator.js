97f94a40e3dcf6bbce30230aca4d3673ddebea28
Greg Mojonnier <gregmojonnier@gmail.com>
2016-06-18 11:53:05 -0400

    Added first 3 tests for sum.

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -1,6 +1,18 @@
-module.exports = function() {
-	return new StringCalculator;
-};
+module.exports = StringCalculator;
 
 function StringCalculator() {
+    this.sum = function(numbers) {
+        if (!numbers) {
+            return 0;
+        }
+        var nums = numbers.split(' ');
+        if (nums.length > 1) {
+            var sum = 0;
+            nums.forEach(function(i) {
+                sum += Number(i);
+            });
+            return sum;
+        }
+        return Number(numbers);
+    };
 }
--- test/string_calculator.js
+++ test/string_calculator.js
@@ -2,9 +2,18 @@ var test = require('tape');
 var Calculator = require('../src/string_calculator');
 
 test('StringCalculator.Constructor', function(t) {
-	var calc1 = Calculator();
-	var calc2 = Calculator();
+	var calc1 = new Calculator();
+	var calc2 = new Calculator();
 
 	t.isNotEqual(calc1, calc2, 'each calculator is a unique instance');
+	t.end();
+});
+
+test('StringCalculator.Sum', function(t) {
+    var calc = new Calculator();
+
+	t.isEqual(calc.sum(''), 0, 'an empty string equals 0');
+	t.isEqual(calc.sum('18'), 18, 'a single number equals itself');
+	t.isEqual(calc.sum('1 8'), 9, 'two space delimited numbers equals their sum');
 	t.end();
 });
