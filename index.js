785ee4c68e29d6c043a1e5be042218ba4f391e04
Greg Mojonnier <gregmojonnier@gmail.com>
2016-06-18 14:41:10 -0400

    Fixed lint errors.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var program = require('commander');
-var string_calculator = require('./src/string_calculator');
+var StringCalculator = require('./src/string_calculator');
 
 program
     .version('1.0.0')
@@ -9,7 +9,7 @@ program
 
 if (program.sum_numbers) {
         console.log('Ready to start summing!');
-        var calculator = new string_calculator();
+        var calculator = new StringCalculator();
         var result = calculator.sum(program.sum_numbers);
         console.log('The sum of', '"' + program.sum_numbers + '"', 'is', result);
 } else {
