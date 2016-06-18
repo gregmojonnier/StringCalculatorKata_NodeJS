36abeaf07aacb66dada8fc81fd08ec9ee11ab3dc
Greg Mojonnier <gregmojonnier@gmail.com>
2016-06-17 23:26:41 -0400

    Made the first test pass.

--- test/string_calculator.js
+++ test/string_calculator.js
@@ -2,8 +2,8 @@ var test = require('tape');
 var calculator = require('../src/string_calculator');
 
 test('Constructing string calculators', function (t) {
-	var calc1 = calculator();
-	var calc2 = calculator();
+	var calc1 = new calculator();
+	var calc2 = new calculator();
 
 	t.isNotEqual(calc1, calc2, 'each calculator is a unique instance')
 	t.end();
